9f0dff7af177c7b0357fd14623e5540d0ed9a838
walon <walon@focus-creative-games.com>
2022-10-02 12:43:45 +0800

    [fix] 修复退出Managed Thread时未清理MachineState造成内存泄漏的bug

--- libil2cpp/vm/Thread.cpp
+++ libil2cpp/vm/Thread.cpp
@@ -34,6 +34,8 @@ extern "C" {
 }
 #endif
 
+#include "hybridclr/interpreter/InterpreterModule.h"
+
 namespace il2cpp
 {
 namespace vm
@@ -228,7 +230,7 @@ namespace vm
 #if IL2CPP_MONO_DEBUGGER
         utils::Debugger::FreeThreadLocalData();
 #endif
-
+        hybridclr::interpreter::InterpreterModule::FreeThreadLocalMachineState();
         os::Thread::DetachCurrentThread();
         s_CurrentThread.SetValue(NULL);
     }
