3d72d21cc68693a4bdc8f1a900f55290a4ab9c65
walon <walon@code-philosophy.com>
2023-10-14 14:04:21 +0800

    [fix] 修复Il2CppGenericContextCompare比较时仅仅对比inst指针的bug，造成热更新模块大量泛型函数重复

--- libil2cpp/metadata/Il2CppGenericInstCompare.cpp
+++ libil2cpp/metadata/Il2CppGenericInstCompare.cpp
@@ -22,6 +22,18 @@ namespace metadata
         return AreEqual(t1.key, t2.key);
     }
 
+    bool Il2CppGenericInstCompare::Compare(const Il2CppGenericInst* t1, const Il2CppGenericInst* t2)
+    {
+        if (t1)
+        {
+            return t2 && AreEqual(t1, t2);
+        }
+        else
+        {
+            return !t2;
+        }
+    }
+
     bool Il2CppGenericInstCompare::AreEqual(const Il2CppGenericInst* t1, const Il2CppGenericInst* t2)
     {
         if (t1->type_argc != t2->type_argc)
