f4c97309176d806e82d0bf571501851434beb428
walon <walon@code-philosophy.com>
2025-03-19 20:49:59 +0800

    [opt] reorder MethodInfo fields to reduce memory size

--- libil2cpp/il2cpp-class-internals.h
+++ libil2cpp/il2cpp-class-internals.h
@@ -354,11 +354,11 @@ typedef struct MethodInfo
     uint8_t has_full_generic_sharing_signature : 1;
 
     // ==={{ hybridclr
+    bool initInterpCallMethodPointer : 1;
+    bool isInterpterImpl : 1;
     void* interpData;
     Il2CppMethodPointer methodPointerCallByInterp;
     Il2CppMethodPointer virtualMethodPointerCallByInterp;
-    bool initInterpCallMethodPointer;
-    bool isInterpterImpl;
     // ===}} hybridclr
 } MethodInfo;
 
